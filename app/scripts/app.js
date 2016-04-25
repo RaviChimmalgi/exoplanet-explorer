d7786cb92d343190b1e9e366a9edfe6419d7ded4
RaviChimmalgi <ravi.chimmalgi@gmail.com>
2016-04-25 03:39:39 -0500

    feat: udacity quiz, first thumbnail

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,6 +64,10 @@ Instructions:
 
     Your code goes here!
      */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response){addSearchHeader(response.query); return getJSON(response.results[0]);})
+    .catch(function() {throw error('Search Error');})
+    .then(function(planetinfo){createPlanetThumb(planetinfo);})
+    .catch(function(error){addSearchHeader('unknown');})
   });
 })(document);
